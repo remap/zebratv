5fbb4973bed7ce83cbf3d623550a90bd0f523071
Alex Nano <alexnano@ucla.edu>
2013-10-04 12:22:51 -0700

    scaled display to better fit REMAP test room

--- data.js
+++ data.js
@@ -1,12 +1,6 @@
 
 connect("128.97.152.30",5117);
 
-/**
- * Connects to a host and port
- *
- * @param {String} host The remote host to connect to
- * @param {Number} port The port to connect to at the remote host
- */
 function connect(host, port) {
   tcpClient = new TcpClient(host, port);
 
@@ -20,8 +14,7 @@ function connect(host, port) {
           e = lines[i].split(',')
           setPosition(e[1],e[2],e[3],e[4])
       }
-      //var output = lines.join('..\n');
-      //log(output)
+
     });
   });
 }
@@ -31,12 +24,9 @@ var sensors = {};
 
 function setPosition(id, x, y ,z){
     oldLength = Object.keys(sensors).length
-    //log(" setting element "+id+" to "+x+","+y+","+z) 
     sensors[id] = {'x':x,'y':y,'z':z,'name':id}
     newLength = Object.keys(sensors).length
     if(newLength>oldLength) addElement(id);
-    //log("elem length is "+Object.keys(elem).length)
-    //log("x is "+elem[id]['x'])
 }
 
 
--- plot.js
+++ plot.js
@@ -1,36 +1,23 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-
+var camera = new THREE.PerspectiveCamera(55, window.innerWidth/window.innerHeight, 0.1, 1000);
+camera.position.x = 2
+camera.position.y = .75
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-//scene.add(cube);
 
 camera.position.z = 5;
 
 var render = function () {
 	requestAnimationFrame(render);
-
-	//cube.rotation.x += 0.005;
-//	cube.rotation.y += 0.001;
-
-    // update sensor tags
-    //scene.getObjectByName()
-
         
     for (sensor in sensors){
         geo = scene.getObjectByName(sensors[sensor].name)
-       // log(geo)
-        geo.position.x = sensors[sensor].x/20
-        geo.position.y = sensors[sensor].y/20
-        geo.position.z = sensors[sensor].z/20
-        //log("set to "+geo.x)
+        geo.position.x = sensors[sensor].x/10
+        geo.position.y = sensors[sensor].y/10
+        geo.position.z = sensors[sensor].z/10
     }
-    
-    //elem[id] = {'x':x,'y':y,'z':z}
-    //log("elem length is "+Object.keys(elem).length)
-    //log("x is "+elem[id]['x'])
 
 	renderer.render(scene, camera);
 	
@@ -39,9 +26,8 @@ var render = function () {
 render();
 
 
-
 var addElement = function(id){
-    var geometry = new THREE.CubeGeometry(.25,.25,.25);
+    var geometry = new THREE.CubeGeometry(.1,.1,.1);
     var material = new THREE.MeshBasicMaterial({color: '#'+Math.floor(Math.random()*16777215).toString(16)});
     var tmpMesh = new THREE.Mesh(geometry, material);
     tmpMesh.name = id;
